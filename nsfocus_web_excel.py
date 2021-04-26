6789d1ee48eead63194249455417d7c16bdb73a6
bozhihouc <852782749@qq.com>
2021-04-26 17:05:51 +0800

    test

---
+++ nsfocus_web_excel.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/26 下午5:02
+# @Author  : 852782749@qq.com
+# @File    : nsfocus_web_excel.py.py
+# @Software: PyCharm
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
